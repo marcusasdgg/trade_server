e4b8f5913a1edafe005fceb0bcb1b7494cd73078
marcusasdgg <marcusolachan@icloud.com>
2024-06-30 18:46:33 +1000

    g

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,13 @@ struct Client {
 }
 pub struct Dominator {
     //clients: Vec<Client>,
+    baseAddress: String,
 }   
 
 impl Dominator {
     pub async fn new(optional_address: &str) //no checking for shitty address rn.
     {
+        let baseAddress = String::from("");
         println!("starting server up!");
         let add = optional_address.to_string();
         tokio::spawn(async {
@@ -23,9 +25,17 @@ impl Dominator {
        
     }
 
+    //function that setups up a handler loop that accepts incoming connections then diverts them to
+    //another assigned port.
+    async fn connectionHandler() -> String{
+
+
+        "a".to_string()
+    }
+
     async fn start_broadcast(address: String){
         println!("starting broadcasting!");
-        let multiaddress = "239.255.255.250:1900".to_string();
+        let multiaddress = "239.255.255.250:1901".to_string();
 
         let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind to address");
         socket.set_multicast_loop_v4(true).expect("Couldn't set multicast loop");
@@ -35,7 +45,7 @@ impl Dominator {
 
         loop {
             socket.send_to(message.as_bytes(), multiaddress.clone()).expect("Couldn't send data");
-            tokio::time::sleep(Duration::from_secs(1)).await;
+            tokio::time::sleep(Duration::from_secs(2)).await;
         }
     }
 
